3efca017cec2f9aa817c3ae9acbc00efc7d1118d
Tomas Dirvanauskas <tomas@unity3d.com>
2016-04-25 14:43:13 +0300

    Fix AssociationLaunching example

--- AssocationLaunch/Assets/AssocationLaunching.cs
+++ AssocationLaunch/Assets/AssocationLaunching.cs
@@ -16,9 +16,13 @@ public class AssocationLaunching : MonoBehaviour
 	// Update is called once per frame
 	void OnGUI ()
 	{
-#if UNITY_METRO
+#if UNITY_WSA
+#if UNITY_EDITOR
+		GUILayout.Label("Build to Windows Store Apps first!");
+#endif
 		GUILayout.Label("Application args: " + UnityEngine.WSA.Application.arguments);
 		GUILayout.Space(15);
+		GUILayout.Label("1. Example: shows how to open an URL in a browser");
 		uri = GUILayout.TextField(uri);
 		if (GUILayout.Button("Launch via Uri"))
 			UnityEngine.WSA.Launcher.LaunchUri(uri, true);
@@ -26,6 +30,7 @@ public class AssocationLaunching : MonoBehaviour
 		GUILayout.Space(15);
 		// Note: myunitygame tag must match with the one in Package.appxmanifest under Protocol field
 		// You can also Wnd+R, and type myunitygame:// , this will also launch this application.
+		GUILayout.Label("2. Example: shows how to open this Windows Store application via protocol, this way you can open this application from another Windows Store application.");
 		if (GUILayout.Button("Launch via Uri with 'myunitygame'"))
 		{
 			// This will invoke OnActivated function in App.xaml.cs
@@ -33,18 +38,21 @@ public class AssocationLaunching : MonoBehaviour
 		}
 		GUILayout.Space(15);
 		// Note: myunitygame extension must match with the one in Package.appxmanifest under File Type Assocations
+		GUILayout.Label("3. Example: shows how to open this Windows Store application via file association. Try creating a file, for ex., 'test.myunitygame', open it, this WSA application should open.");
 		if (GUILayout.Button("Launch via File '" + assocationFilePath + "'"))
 		{
 			// This will invoke OnFileActivated function in App.xaml.cs
 			UnityEngine.WSA.Launcher.LaunchFile(UnityEngine.WSA.Folder.Installation, assocationFilePath, true);
 		}
 		GUILayout.Space(15);
+		GUILayout.Label(string.Format("4. Example: open an application associated {0} extension (by default it's notepad.exe)", extension));
 		extension = GUILayout.TextField(extension);
 		if (GUILayout.Button("Launch via File Picker"))
 			UnityEngine.WSA.Launcher.LaunchFileWithPicker(extension);
 
 		GUILayout.Space(15);
 		relativeFilePath = GUILayout.TextField(relativeFilePath);
+		GUILayout.Label("5. Example: shows how to open a file from application data folder");
 		if (GUILayout.Button("Launch via File"))
 			UnityEngine.WSA.Launcher.LaunchFile(UnityEngine.WSA.Folder.Installation, relativeFilePath, true);
 #else
