940d04c028796ba865c1171fb12f303b72c38e15
Tomas Dirvanauskas <tomas@unity3d.com>
2016-04-25 13:44:40 +0300

    Remove unused files

--- AssocationLaunch/ZipAndCopy.cmd
+++
@@ -1,3 +0,0 @@
-..\..\..\..\tools\doctools\win32\7z\7z.exe a -tzip AssocationLaunch Assets\* ProjectSettings\*
-copy AssocationLaunch.zip ..\..\..\uploads\Main\AssocationLaunch.zip 
-pause
--- MultiplePages/ZipAndCopy.cmd
+++
@@ -1,3 +0,0 @@
-..\..\..\..\tools\doctools\win32\7z\7z.exe a -tzip MultiplePages @FileList.txt
-copy MultiplePages.zip ..\..\..\uploads\Main\MultiplePages.zip 
-pause
